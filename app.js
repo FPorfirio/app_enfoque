1782f476706e367b8b8c73f38e6ef1c4a43d4448
Facundo Porfirio <facundojporfirio@gmail.com>
2020-07-28 17:24:16 -0300

    refactorize y modularice la app

--- app.js
+++ app.js
@@ -1,24 +1,9 @@
+import generadorObjectos from "./funciones.js"
+
+const Contenedor = document.getElementById('contenedor')
 const Colores = ["red", "green", "blue", "yellow", "black"];
-const Palabras = [,];
-Palabras[0] = ["ROJO", "VERDE", "AZUL", "AMARILLO", "NEGRO", "BLANCO"];
-Palabras[1] = ["LIBRE", "SUELTO", "AQUI", "AHORA", "SOY"];
-const PalabrasSalidas = [];
-const snd = new Audio("./metronomo.mp3");
-let milisegundos = 1000;
-const speedContainer = document.querySelector("#speedContainer");
+const Palabras = ["ROJO", "VERDE", "AZUL", "AMARILLO", "NEGRO", "BLANCO", "LIBRE", "SUELTO", "AQUI", "AHORA", "SOY"]
 
-let timer;
-let inicio = false;
-let count = 0;
-
-const PalabraH1 = document.querySelector("h1");
-const startBtn = document.querySelector(".start");
-const stopBtn = document.querySelector(".stop");
-
-startBtn.addEventListener("click", (e)=> {
-  stop();
-  contadorInicial(3);
-});
-
-stopBtn.addEventListener("click", stop);
-
+const merge = [Palabras, Colores]
+console.log(generadorObjectos)
+generadorObjectos(merge, Contenedor )
--- funciones.js
+++ funciones.js
@@ -1,68 +1,141 @@
+const GeneradorObjetos = (function(){
 
-function azar(colores, palabras) {
-  const color = Math.floor(Math.random() * colores.length);
-  const indice = Math.floor(Math.random() * palabras.length);
-  const subIndice = Math.floor(Math.random() * palabras[indice].length);
-  return { palabra: palabras[indice][subIndice], color: colores[color] };
-}
-
-function render(obj) {
-  PalabraH1.className = "";
-  PalabraH1.classList.add(obj.color);
-  PalabraH1.textContent = obj.palabra;
-}
-
-function sound() {
-  snd.pause();
-  snd.load();
-  snd.play();
-}
-
-function start() {
-  speedContainer.classList.add("hidden");
-  PalabraH1.className = "";
-  const Celda = azar(Colores, Palabras);
-  render(Celda);
-  sound();
-  puntaje(Celda.color);
-  const multiplo = count % 30;
-  if (count > 0 && multiplo == 0) {
-  	contadorInicial(4);
+  function GeneradorObjetos(set, contenedor){
+    this.palabras = set[0];
+    this.colores = set[1];
+    this.contenedor = contenedor;
+    this._init();
+    console.log(this.palabraH1)
   }
-}
 
-function puntaje(obj) {
-  PalabrasSalidas[count] =  obj;
-  console.log(PalabrasSalidas[count]);
-  count ++;
-}
+  console.log(GeneradorObjetos)
 
-function time() {
-   // procesar(); 
-  timer = setInterval(start, milisegundos);
-}
+  GeneradorObjetos.prototype = {
 
-function stop(){
-  // procesar();
-  clearInterval(timer);
-  inicio = false;
-  speedContainer.classList.remove("hidden");
-  count = 0;
-  PalabraH1.className = "";
-  PalabraH1.textContent = "--";
-}
+    _init: function(){
+      this._config();
+      this._initEvents()
+    },
+    
+    _azar: function(){
+      const self = this;
+      console.log(this, self)
+      const color = Math.floor(Math.random() * this.colores.length);
+      const indice = Math.floor(Math.random() * this.palabras.length);
+      const subIndice = Math.floor(Math.random() * this.palabras[indice].length);
+      return { palabra: this.palabras[indice], color: this.colores[color] };
+    },
 
-function contadorInicial(i) {
-  milisegundos = speedContainer.querySelector("input").value || 1000;
-	clearInterval(timer);
-  timer = setTimeout(function() {
-    PalabraH1.textContent = i;
-    PalabraH1.className = "";
-    i--;
-    if(i>0) {contadorInicial(i);}
-    else {
-      inicio = true;
-      time(); 
-    }  
-  }, milisegundos);
-}
+    _puntaje: function(obj) {
+     
+    },
+
+    _render: function(renderObj){
+      const self = this;
+      console.log(typeof renderObj)
+
+      self.PalabraH1.className = "";
+      if(typeof renderObj == 'number'){
+        this.PalabraH1.textContent = renderObj
+      }
+      else{
+        this.speedContainer.classList.add('hidden');
+        this.PalabraH1.classList.add(renderObj.color);
+        this.PalabraH1.textContent = renderObj.palabra;
+      }
+    },
+
+    _config: function(){
+      this.secuenciador;
+      this.conteoInicial = 3;
+      this.delay = 1;
+      this.speedContainer = this.contenedor.querySelector("#speedContainer");
+      this.PalabraH1 = this.contenedor.querySelector("h1");
+      this.startBtn = this.contenedor.querySelector(".start");
+      this.stopBtn = this.contenedor.querySelector(".stop")
+    },
+
+    _initEvents: function(){
+      const self = this;
+      this.startBtn.addEventListener('click', function(){
+        self._start()
+      });
+      this.stopBtn.addEventListener('click', function(){
+        self._stop()
+      })
+    },
+
+    _sound: function() {
+      let snd = new Audio("./metronomo.mp3");
+      snd.pause();
+      snd.load();
+      snd.play();
+    },
+
+    _start: function() {
+      const self = this;
+      let start = Date.now();
+      let diff;
+      let minutes;
+      let seconds;
+      function timer() {
+          // get the number of seconds that have elapsed since 
+          // startTimer() was called
+          diff = self.conteoInicial - (((Date.now() - start) / 1000) | 0);
+    
+          // does the same job as parseInt truncates the float
+          minutes = (diff / 60) | 0;
+          seconds = (diff % 60) | 0;
+    
+          minutes = minutes < 10 ? "0" + minutes : minutes;
+          seconds = seconds < 10 ? "0" + seconds : seconds;
+
+          console.log(minutes, seconds, diff)
+          self._render(diff)
+        
+          if (diff <= 0) {
+              // add one second so that the count down starts at the full duration
+              // example 05:00 not 04:59
+              start = Date.now() + 1000;
+              clearInterval(self.secuenciador)
+          }
+      };
+      // we don't want to wait a full second before the timer starts
+      timer();
+    
+      const initialCountdown =  new Promise((resolve, reject) => {
+        self.secuenciador = setInterval(() => {
+          timer();
+          if(diff <= 0){
+            resolve('Done');
+          }
+        },1000)
+      })
+      
+      
+      initialCountdown.then(() => {
+        console.log('here')
+        self.secuenciador = setInterval(() => {
+          self._render(self._azar());
+          self._sound();
+        },1000)
+      })
+    },
+
+    _stop: function(){
+      self = this;
+      console.log(this.secuenciador, self.secuenciador)
+      clearInterval(self.secuenciador);
+    }
+  }
+
+  return function(set, contenedor){
+    var a = new GeneradorObjetos(set, contenedor);
+    console.log(a)
+  }
+
+})()
+
+
+export default GeneradorObjetos;
+
